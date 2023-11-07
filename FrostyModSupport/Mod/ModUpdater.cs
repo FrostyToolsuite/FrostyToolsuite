9e0bc98dcf5154236ab2a359bf6b54807d22bd30
jona <93538252+wannkunstbeikor@users.noreply.github.com>
2023-11-07 13:30:03 +0100

    [ModSupport] More work on ModUpdater

--- FrostyModSupport/Mod/ModUpdater.cs
+++ FrostyModSupport/Mod/ModUpdater.cs
@@ -14,9 +14,9 @@ public class ModUpdater
         Success = 0,
         InvalidMods = -1,
     }
-    
+
     private static Dictionary<int, HashSet<int>> s_bundleMapping = new();
-    
+
     public static Errors UpdateMod(string inPath)
     {
         (BaseModResource[], Block<byte>[], FrostyModDetails, uint)? mod;
@@ -31,7 +31,7 @@ public class ModUpdater
         {
             return Errors.InvalidMods;
         }
-        
+
         using (BlockStream stream = BlockStream.FromFile(inPath, false))
         {
             // read header
@@ -50,42 +50,42 @@ public class ModUpdater
         {
             return Errors.InvalidMods;
         }
-        
+
         FrostyMod.Save(inPath, mod.Value.Item1, mod.Value.Item2, mod.Value.Item3, mod.Value.Item4);
 
         foreach (Block<byte> block in mod.Value.Item2)
         {
             block.Dispose();
         }
-        
+
         return Errors.Success;
     }
 
     private static (BaseModResource[], Block<byte>[], FrostyModDetails, uint)? UpdateNewFormat(DataStream inStream)
     {
         uint version = inStream.ReadUInt32();
-        
+
         long dataOffset = inStream.ReadInt64();
         int dataCount = inStream.ReadInt32();
 
-        if (ProfilesLibrary.ProfileName.Equals(inStream.ReadSizedString(), StringComparison.OrdinalIgnoreCase))
+        if (!ProfilesLibrary.ProfileName.Equals(inStream.ReadSizedString(), StringComparison.OrdinalIgnoreCase))
         {
             return null;
         }
-        
+
         (BaseModResource[], Block<byte>[], FrostyModDetails, uint) retVal = default;
-        retVal.Item4 = inStream.ReadUInt32(); 
-        
+        retVal.Item4 = inStream.ReadUInt32();
+
         retVal.Item3 = new FrostyModDetails(inStream.ReadNullTerminatedString(), inStream.ReadNullTerminatedString(),
             inStream.ReadNullTerminatedString(), inStream.ReadNullTerminatedString(), inStream.ReadNullTerminatedString(),
             version > 4 ? inStream.ReadNullTerminatedString() : string.Empty);
-        
+
         int resourceCount = inStream.ReadInt32();
         BaseModResource[] resources = new BaseModResource[resourceCount];
         for (int i = 0; i < resourceCount; i++)
         {
             ModResourceType type = (ModResourceType)inStream.ReadByte();
-            (int, string, Sha1, long, int, string, IEnumerable<int>) b;
+            (int ResourceIndex, string Name, Sha1 Sha1, long OriginalSize, int HandlerHash, string UserData, IEnumerable<int> BundlesToAdd) b;
             BaseModResource.ResourceFlags flags;
             switch (type)
             {
@@ -98,20 +98,21 @@ public class ModUpdater
                     inStream.ReadNullTerminatedString();
                     int superBundleHash = inStream.ReadInt32();
                     // TODO: update hash and add bundle hash to s_bundleMapping
-                    
+
                     resources[i] = new BundleModResource(b.Item2, superBundleHash);
                     break;
                 case ModResourceType.Ebx:
                     b = ReadBaseModResource(inStream, version);
                     flags = AssetManager.GetEbxAssetEntry(b.Item2) is not null ? BaseModResource.ResourceFlags.IsAdded : 0;
-                    
-                    resources[i] = new EbxModResource(b.Item1, b.Item2, b.Item3, b.Item4, flags, b.Item5, b.Item6, b.Item7, Enumerable.Empty<int>());
+
+                    // we now store ebx names the same way they are stored in the bundle, so all lowercase
+                    resources[i] = new EbxModResource(b.ResourceIndex, b.Name.ToLower(), b.Sha1, b.OriginalSize, flags, b.HandlerHash, b.UserData, b.BundlesToAdd, Enumerable.Empty<int>());
                     break;
                 case ModResourceType.Res:
                     b = ReadBaseModResource(inStream, version);
                     flags = AssetManager.GetResAssetEntry(b.Item2) is not null ? BaseModResource.ResourceFlags.IsAdded : 0;
-                    
-                    resources[i] = new ResModResource(b.Item1, b.Item2, b.Item3, b.Item4, flags, b.Item5, b.Item6, b.Item7,
+
+                    resources[i] = new ResModResource(b.ResourceIndex, b.Name, b.Sha1, b.OriginalSize, flags, b.HandlerHash, b.UserData, b.BundlesToAdd,
                         Enumerable.Empty<int>(), inStream.ReadUInt32(), inStream.ReadUInt64(),
                         inStream.ReadBytes(inStream.ReadInt32()));
                     break;
@@ -125,22 +126,22 @@ public class ModUpdater
                     uint logicalSize = inStream.ReadUInt32();
                     int h32 = inStream.ReadInt32();
                     int firstMip = inStream.ReadInt32();
-                    
+
                     IEnumerable<int> superBundlesToAdd = Enumerable.Empty<int>();
                     if (flags.HasFlag(BaseModResource.ResourceFlags.IsAdded))
                     {
                         // TODO: add to superbundle
                     }
 
-                    resources[i] = new ChunkModResource(b.Item1, b.Item2, b.Item3, b.Item4, flags, b.Item5, b.Item6,
-                        b.Item7, Enumerable.Empty<int>(), rangeStart, rangeEnd, logicalOffset, logicalSize, h32,
+                    resources[i] = new ChunkModResource(b.ResourceIndex, b.Name, b.Sha1, b.OriginalSize, flags, b.HandlerHash, b.UserData,
+                        b.BundlesToAdd, Enumerable.Empty<int>(), rangeStart, rangeEnd, logicalOffset, logicalSize, h32,
                         firstMip, superBundlesToAdd, Enumerable.Empty<int>());
                     break;
                 default:
                     throw new Exception("Unexpected mod resource type");
             }
         }
-        
+
         return retVal;
     }
 
@@ -153,7 +154,202 @@ public class ModUpdater
             return null;
         }
 
-        return default;
+        if (!ProfilesLibrary.ProfileName.Equals(mod.AsString("gameProfile"), StringComparison.OrdinalIgnoreCase))
+        {
+            return null;
+        }
+
+        int version = int.Parse(mod.AsString("magic").Remove(0, 6));
+
+        FrostyModDetails modDetails = new(mod.AsString("title"), mod.AsString("author"), mod.AsString("category"),
+            mod.AsString("version"), mod.AsString("description"), string.Empty);
+
+        Dictionary<int, (HashSet<int>, HashSet<int>)> bundles = new();
+        foreach (DbObject actionObj in mod.AsList("actions"))
+        {
+            DbObjectDict action = actionObj.AsDict();
+            string bundleName = action.AsString("bundle");
+            int bundleHash = 0; // TODO: calculate bundle hash
+            string type = action.AsString("type");
+            int resourceId = action.AsInt("resourceId");
+
+            switch (type)
+            {
+                case "modify":
+                    // we dont really need to do anything since we get the modified bundles through the AssetManager when applying mods
+                    break;
+                case "add":
+                    bundles.TryAdd(resourceId, (new HashSet<int>(), new HashSet<int>()));
+                    bundles[resourceId].Item1.Add(bundleHash);
+                    break;
+                case "remove":
+                    bundles.TryAdd(resourceId, (new HashSet<int>(), new HashSet<int>()));
+                    bundles[resourceId].Item2.Add(bundleHash);
+                    break;
+            }
+        }
+
+        int id = 0, i = 0, resourceIndex = -1;
+
+        DbObjectList resourcesList = mod.AsList("resources");
+        List<BaseModResource> resources = new(resourcesList.Count);
+        List<Block<byte>> data = new(resourcesList.Count);
+        Dictionary<int, BlockStream> archiveStreams = new();
+
+        // create mandatory resources for icon and 4 screenshots
+        int index = mod.AsInt("icon", -1);
+        if (index != -1)
+        {
+            DbObjectDict icon = resourcesList[index].AsDict();
+            resourceIndex = ReadArchive(data, archiveStreams, icon, modName);
+        }
+
+        resources.Add(new EmbeddedModResource(resourceIndex, "Icon"));
+
+        resourceIndex = -1;
+        DbObjectList screenshots = mod.AsList("screenshots");
+        for (int j = 0; j < 4; j++)
+        {
+            if (j < screenshots.Count)
+            {
+                index = screenshots[j].AsInt();
+                if (index != -1)
+                {
+                    resourceIndex = ReadArchive(data, archiveStreams, resourcesList[index].AsDict(), modName);
+                }
+                else
+                {
+                    resourceIndex = -1;
+                }
+            }
+
+            resources.Add(new EmbeddedModResource(resourceIndex, $"Screenshot{j}"));
+        }
+
+        resourceIndex = -1;
+        foreach (DbObject resourceObj in resourcesList)
+        {
+            DbObjectDict resource = resourceObj.AsDict();
+            string type = resource.AsString("type");
+            string name = resource.AsString("name");
+            Sha1 sha1 = resource.AsSha1("sha1");
+
+            IEnumerable<int> bundlesToAdd;
+            IEnumerable<int> bundlesToRemove;
+            if (bundles.TryGetValue(id, out (HashSet<int>, HashSet<int>) b))
+            {
+                bundlesToAdd = b.Item1;
+                bundlesToRemove = b.Item2;
+            }
+            else
+            {
+                bundlesToAdd = Enumerable.Empty<int>();
+                bundlesToRemove = Enumerable.Empty<int>();
+            }
+
+            if (type != "embedded")
+            {
+                resourceIndex = ReadArchive(data, archiveStreams, resource, modName);
+            }
+
+            switch (type)
+            {
+                case "superbundle":
+                    // not supported yet
+                    break;
+                case "bundle":
+                    throw new NotImplementedException();
+                case "ebx":
+                {
+                    BaseModResource.ResourceFlags flags = AssetManager.GetEbxAssetEntry(name) is null
+                        ? BaseModResource.ResourceFlags.IsAdded
+                        : 0;
+                    resources.Add(new EbxModResource(resourceIndex, name, sha1, resource.AsLong("uncompressedSize"),
+                        flags, 0, string.Empty, bundlesToAdd, bundlesToRemove));
+                    break;
+                }
+                case "res":
+                {
+                    BaseModResource.ResourceFlags flags = AssetManager.GetResAssetEntry(name) is null
+                        ? BaseModResource.ResourceFlags.IsAdded
+                        : 0;
+                    resources.Add(new ResModResource(resourceIndex, name, sha1, resource.AsLong("uncompressedSize"),
+                        flags, 0, string.Empty, bundlesToAdd, bundlesToRemove, resource.AsUInt("resType"),
+                        resource.AsULong("resRid"), resource.AsBlob("resMeta")));
+                    break;
+                }
+                case "chunk":
+                {
+                    Guid chunkId = Guid.Parse(name);
+
+                    BaseModResource.ResourceFlags flags;
+                    IEnumerable<int> superBundlesToAdd;
+                    if (AssetManager.GetChunkAssetEntry(chunkId) is null)
+                    {
+                        flags = BaseModResource.ResourceFlags.IsAdded;
+                        // TODO: add to some superbundles
+                        superBundlesToAdd = Enumerable.Empty<int>();
+                    }
+                    else
+                    {
+                        flags = 0;
+                        superBundlesToAdd = Enumerable.Empty<int>();
+                    }
+
+                    uint rangeStart = resource.AsUInt("rangeStart");
+                    uint rangeEnd = resource.AsUInt("rangeEnd");
+                    uint logicalOffset = resource.AsUInt("logicalOffset");
+                    uint logicalSize = resource.AsUInt("logicalSize");
+                    int h32 = resource.AsInt("h32");
+                    int firstMip = resource.AsInt("firstMip");
+
+                    if ((logicalOffset != 0 && rangeStart == 0) || rangeEnd == 0)
+                    {
+                        // TODO: calculate range
+                    }
+
+                    // TODO: version shit
+
+                    resources.Add(new ChunkModResource(resourceIndex, name, sha1, resource.AsLong("uncompressedSize"),
+                        flags, 0, string.Empty, bundlesToAdd, bundlesToRemove, rangeStart, rangeEnd, logicalOffset,
+                        logicalSize, h32, firstMip, superBundlesToAdd, Enumerable.Empty<int>()));
+                    break;
+                }
+            }
+
+            id++;
+        }
+
+        return (resources.ToArray(), data.ToArray(), modDetails, mod.AsUInt("gameVersion"));
+    }
+
+    private static int ReadArchive(List<Block<byte>> data, Dictionary<int, BlockStream> archiveStreams,
+        DbObjectDict inDict, string inModName)
+    {
+        int archiveIndex = inDict.AsInt("archiveIndex", -1);
+        if (archiveIndex == -1)
+        {
+            return -1;
+        }
+
+        if (!archiveStreams.TryGetValue(archiveIndex, out BlockStream? stream))
+        {
+            string path = $"{inModName}_{archiveIndex:D2}.archive";
+            if (!File.Exists(path))
+            {
+                return -1;
+            }
+            stream = BlockStream.FromFile(path, false);
+        }
+
+        uint archiveOffset = inDict.AsUInt("archiveOffset");
+        stream.Position = archiveOffset;
+        Block<byte> block = new(inDict.AsInt("compressedSize"));
+        stream.ReadExactly(block);
+
+        data.Add(block);
+
+        return data.Count - 1;
     }
 
     private static (int, string, Sha1, long, int, string, IEnumerable<int>) ReadBaseModResource(DataStream inStream, uint version)
@@ -172,7 +368,7 @@ public class ModUpdater
             retVal.Item5 = inStream.ReadInt32();
             retVal.Item6 = version > 2 ? inStream.ReadNullTerminatedString() : string.Empty;
         }
-        
+
         // prior to version 4, mods stored bundles the asset already existed in for modification
         // so must read and ignore this list
         if (version < 4 && retVal.Item1 != -1)
@@ -203,7 +399,7 @@ public class ModUpdater
             }
             retVal.Item7 = bundles;
         }
-        
+
         return retVal;
     }
 }
