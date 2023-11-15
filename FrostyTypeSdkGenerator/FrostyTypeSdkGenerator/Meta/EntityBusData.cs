ede8375b3a6f90aebf948d7222bb30f4bfc9b904
jona <93538252+wannkunstbeikor@users.noreply.github.com>
2023-11-15 10:46:30 +0100

    [TypeSdkGenerator] Fixed wrong flag

--- FrostyTypeSdkGenerator/FrostyTypeSdkGenerator/Meta/EntityBusData.cs
+++ FrostyTypeSdkGenerator/FrostyTypeSdkGenerator/Meta/EntityBusData.cs
@@ -6,15 +6,15 @@ public partial class EntityBusData
     [EbxFieldMetaAttribute(TypeFlags.TypeEnum.Boolean)]
     public bool EntityBusClientFlag
     {
-        get => (Flags & 0x8u) != 0;
-        set => Flags = (ushort)(value ? Flags | 0x8u : Flags & ~0x8u);
+        get => (Flags & 0x4u) != 0;
+        set => Flags = (ushort)(value ? Flags | 0x4u : Flags & ~0x4u);
     }
 
     [IsTransientAttribute()]
     [EbxFieldMetaAttribute(TypeFlags.TypeEnum.Boolean)]
     public bool EntityBusServerFlag
     {
-        get => (Flags & 0x10u) != 0;
-        set => Flags = (ushort)(value ? Flags | 0x10u : Flags & ~0x10u);
+        get => (Flags & 0x8u) != 0;
+        set => Flags = (ushort)(value ? Flags | 0x8u : Flags & ~0x8u);
     }
 }
