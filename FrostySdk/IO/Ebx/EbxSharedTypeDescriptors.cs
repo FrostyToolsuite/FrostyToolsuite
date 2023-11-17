0a1de6678022a6d0dc13842759ae1eb976c643dc
jona <93538252+wannkunstbeikor@users.noreply.github.com>
2023-11-17 16:27:16 +0100

    [Sdk] Fixed crash with SharedTypeDescriptors

--- FrostySdk/IO/Ebx/EbxSharedTypeDescriptors.cs
+++ FrostySdk/IO/Ebx/EbxSharedTypeDescriptors.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Frosty.Sdk.Managers;
 using Frosty.Sdk.Utils;
 
@@ -114,7 +113,9 @@ public static class EbxSharedTypeDescriptors
                 {
                     // its a relative offset to the field, so we have to calculate the index
                     typeDescriptor.FieldIndex = (int)((offset - typeDescriptor.FieldIndex - 0x08) / 0x10 + startFields);
+                    s_keyTypeMapping.Remove(key);
                     s_keyTypeMapping.Add(key, s_typeDescriptors.Count);
+                    s_typeKeyMapping.Remove(typeDescriptor.NameHash);
                     s_typeKeyMapping.Add(typeDescriptor.NameHash, key);
                 }
 
