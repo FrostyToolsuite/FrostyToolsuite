d639c9c3267f4118cdc28d0f739e5caa216177e2
jona <93538252+wannkunstbeikor@users.noreply.github.com>
2024-01-03 22:05:09 +0100

    [TypeSdkGenerator] Improve meta handling

--- FrostyTypeSdkGenerator/FrostyTypeSdkGenerator/Meta/Asset.cs
+++ FrostyTypeSdkGenerator/FrostyTypeSdkGenerator/Meta/Asset.cs
@@ -2,6 +2,7 @@ namespace Frosty.Sdk.Ebx;
 
 public partial class Asset
 {
+    [OverrideAttribute]
     [IsReadOnlyAttribute()]
     [CategoryAttribute("Annotations")]
     public CString Name { get; set; }
