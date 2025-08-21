302d57374bb81015e1c41d70127c8be077d6a855
unelegal <me@unelegal.at>
2025-08-21 16:15:10 +0200

    [Sdk] Don't crash when cas file does not exist
    BF2042 is missing a cas file for some reason

--- FrostySdk/Managers/AssetManager.cs
+++ FrostySdk/Managers/AssetManager.cs
@@ -585,9 +585,17 @@ public static class AssetManager
             }
             else if (entry.LogicalSize == 0)
             {
-                a++;
-                entry.OriginalSize = entry.FileInfo.GetOriginalSize();
-                entry.LogicalSize = (uint)entry.OriginalSize;
+                try
+                {
+                    entry.OriginalSize = entry.FileInfo.GetOriginalSize();
+                    entry.LogicalSize = (uint)entry.OriginalSize;
+                    a++;
+                }
+                catch (FileNotFoundException)
+                {
+                    s_chunkGuidMapping.Remove(entry.Id);
+                    FrostyLogger.Logger?.LogWarning($"Skipping chunk {entry.Id}, bc the file it's stored in does not exist!");
+                }
             }
         }
         FrostyLogger.Logger?.LogInfo($"Had to resolve OriginalSize for {a} chunks");
