f152452a931471fcf9570ec277bc42e3331fd833
unelegal <me@unelegal.at>
2025-08-22 19:19:05 +0200

    [Sdk] Fix fields & primitives being empty

--- FrostySdk/Sdk/FieldInfo.cs
+++ FrostySdk/Sdk/FieldInfo.cs
@@ -65,16 +65,18 @@ internal class FieldInfo : IComparable
 
         p_typeInfo = reader.ReadLong();
 
-        if (ProfilesLibrary.HasStrippedTypeNames && Strings.HasStrings &&
-            Strings.FieldMapping!.TryGetValue(inTypeHash, out Dictionary<uint, string>? dict) &&
-            dict.TryGetValue(m_nameHash, out string? resolvedName))
+        if (ProfilesLibrary.HasStrippedTypeNames)
         {
-            Debug.Assert(!string.IsNullOrEmpty(resolvedName));
-            m_name = resolvedName;
-        }
-        else
-        {
-            m_name = $"Field_{m_nameHash:x8}";
+            if (Strings.HasStrings && Strings.FieldMapping!.TryGetValue(inTypeHash, out Dictionary<uint, string>? dict) &&
+                dict.TryGetValue(m_nameHash, out string? resolvedName))
+            {
+                Debug.Assert(!string.IsNullOrEmpty(resolvedName));
+                m_name = resolvedName;
+            }
+            else
+            {
+                m_name = $"Field_{m_nameHash:x8}";
+            }
         }
     }
 
